c88c967bebaad04bc597dde26cbec56e497f99d9
grizeus <zaytsewww@gmail.com>
2024-07-17 21:59:53 +0300

    complete 1 task

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,17 @@
 "use strict";
 
-const numCategories = document.querySelector("ul#categories");
-console.log(numCategories);
+const categories = document.querySelector("#categories");
+const categoriesChildrens =  categories.children;
+const childrenNum =  categoriesChildrens.length;
+
+console.log(`Number of categories: ${childrenNum}`);
+
+for (const child of categoriesChildrens) {
+  const heading = child.querySelector("h2");
+  const headingText = heading.textContent;
+  const innerUl = child.querySelector("ul");
+  const innerUlChildrenNum = innerUl.children.length;
+
+  console.log(`Category: ${headingText}`);
+  console.log(`Elements: ${innerUlChildrenNum}`);
+}
