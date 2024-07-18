92971f13f3f1a17fa1d6a7476cd43022372787e9
grizeus <zaytsewww@gmail.com>
2024-07-18 14:55:21 +0300

    add boiler to js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("input", event => {
+  event.preventDefault();
+    const curInput = event.currentTarget.value.trim();
+    console.log(curInput);
+    if (curInput === "") {
+        alert("All form fields must be filled in");
+    }
+//   if (curInput !== "") {
+//     output.textContent = curInput;
+//   } else {
+//     output.textContent = "Anonymous";
+//   }
+});
