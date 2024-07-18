2d97d385a662d7a2af079b015146479d1d315638
grizeus <zaytsewww@gmail.com>
2024-07-18 23:46:24 +0300

    add logic task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const button = body.querySelector(".change-color");
+const span = body.querySelector(".color");
+
+button.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+});
